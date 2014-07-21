55dd5f84a3dfff75a4af5fcbbbc5380279e41e3f
Yann Schepens <yann.schepens@gmail.com>
2014-07-21 16:38:47 +0200

    Add base test class

---
+++ src/Kmelia/FreshBundle/Tests/BaseTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kmelia\FreshBundle\Tests;
+
+class BaseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddition()
+    {
+        $result = 1 + 1;
+
+        $this->assertEquals(2, $result);
+    }
+}
