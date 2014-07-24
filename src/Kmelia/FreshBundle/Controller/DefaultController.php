681be888efd40142a78ae732f8e65405ba360065
Maxime <maxoo.design@free.fr>
2014-07-24 14:08:23 +0200

    override render method with a default response (5 minutes of caching) if nothing is specified

--- src/Kmelia/FreshBundle/Controller/DefaultController.php
+++ src/Kmelia/FreshBundle/Controller/DefaultController.php
@@ -3,7 +3,56 @@
 namespace Kmelia\FreshBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    const DEFAULT_DURATION = 300;
+    
+    /**
+     * Renders a view with a default Reponse
+     * @see \Symfony\Bundle\FrameworkBundle\Controller\Controller::render()
+     */
+    public function render($view, array $parameters = array(), Response $response = null)
+    {
+        if( ! $response instanceof Response )
+        {
+            $response = $this->buildDefaultResponse();
+        }
+        
+        return parent::render($view, $parameters, $response);
+    }
+    
+    /**
+     * Build a Response with HTTP cache headers
+     * @see http://symfony.com/fr/doc/current/book/http_cache.html
+     */
+    protected function buildDefaultResponse()
+    {
+        $duration = $this->getCacheDuration();
+        
+        $response = new Response();
+        
+        // mark the response as private
+        $response->setPrivate();
+        
+        // set the private or shared max age
+        $response->setMaxAge($duration);
+        $response->setSharedMaxAge($duration);
+        
+        // set expires
+        $date = new \DateTime();
+        $date->modify(sprintf('+%d seconds', $duration));
+        $response->setExpires($date);
+        
+        // set a custom Cache-Control directive
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+        
+        return $response;
+    }
+    
+    protected function getCacheDuration()
+    {
+        return self::DEFAULT_DURATION;
+    }
 }
