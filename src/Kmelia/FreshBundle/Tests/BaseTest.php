ce3e0ec57895a233666d470d1ed7109a87652876
Maxime <maxoo.design@free.fr>
2014-07-25 09:50:42 +0200

    add test file

--- src/Kmelia/FreshBundle/Tests/BaseTest.php
+++ src/Kmelia/FreshBundle/Tests/BaseTest.php
@@ -10,4 +10,9 @@ class BaseTest extends \PHPUnit_Framework_TestCase
         
         $this->assertEquals(2, $result);
     }
+    
+    public function testFile()
+    {
+        $this->assertFileExists('./vendor/autoload.php');
+    }
 }
