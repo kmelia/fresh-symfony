40b4aca2eac8982500e1cabe84f1bc052f16b993
Maxime <maxoo.design@free.fr>
2014-07-25 09:50:11 +0200

    modify test name

--- src/Kmelia/FreshBundle/Tests/BaseFunctionalTest.php
+++ src/Kmelia/FreshBundle/Tests/BaseFunctionalTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BaseFunctionalTest extends WebTestCase
 {
-    public function testIndex()
+    public function testHomepage()
     {
         $client = static::createClient();
         
