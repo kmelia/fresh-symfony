aeec67f66de5b3588291c75f12c4a7099a31f535
Maxime <maxoo.design@free.fr>
2014-07-25 11:48:23 +0200

    remove unused class

--- src/Kmelia/FreshBundle/Controller/KmeliaController.php
+++ src/Kmelia/FreshBundle/Controller/KmeliaController.php
@@ -3,7 +3,6 @@
 namespace Kmelia\FreshBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 abstract class KmeliaController extends AbstractController
 {
